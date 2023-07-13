3a8afe634ffc7e7d0c5f222a088dd3d1532e0601
KRahul19 <124139730+KRahul19@users.noreply.github.com>
2023-07-13 07:06:42 +0000

    commit

--- modile-2/maps.go
+++ modile-2/maps.go
@@ -1,18 +1,36 @@
 package main
+
 import "fmt"
 
 func main() {
-         var prev,current,sum int
-         prev=0
-         current=1
-        sum=0
-    for prev+current <40{
-        next:= prev+current
-        if next%2==0 {
-            sum+=next
-        }
-        prev=current
-        current=next
-    }
-     fmt.Println("The sum of the even valued term is",sum)
+	//declare and initialise map
+	myMap := make(map[string]int)
+	myMap["apple"] = 1
+	myMap["banana"] = 2
+	myMap["orange"] = 3
+	//Accessig values using keys
+	appleValue := myMap["apple"]
+	bananaValue := myMap["banana"]
+	fmt.Println("Value of apple", appleValue)
+	fmt.Println("Value of banana", bananaValue)
+	//updating values
+	myMap["apple"] = 5
+	fmt.Println("Updated value of apple", myMap["apple"])
+	//Deleting a keyvalue pair
+	delete(myMap, "orange")
+	fmt.Println("After deleting orange:", myMap)
+	//checking existence of a key
+	value, exists := myMap["banana"]
+	if exists {
+		fmt.Println("Value of banana:", value)
+	} else {
+		fmt.Println("Banana not found in the map")
+	}
+	//iterating over the map
+	for key, value := range myMap {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+	//Length of the map
+	fmt.Println("Length of the map:",len(myMap))
+
 }
